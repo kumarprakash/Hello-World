0e247a3cf48283c009922af5d53f6310880ab95a
kumarprakash <kumarprakash@users.noreply.github.com>
2017-06-06 17:49:28 +0530

    Add files via upload

---
+++ app/code/local/Prakash/Mymodule/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Prakash_Mymodule_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        echo "This is my First Module";
+    }
+
+}
